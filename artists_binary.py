cca800c1823e47a4390054bc75a0237211ab644a
Conrad Shock <iconshocking>
2015-04-08 15:58:16 -0400

    can now determine number of most common tags to use for binary vector

--- artists_binary.py
+++ artists_binary.py
@@ -4,6 +4,7 @@ from sklearn import preprocessing
 
 tag_file = 'artists_tags.csv'
 bin_file = 'artists_bin.csv'
+num_tags = 50
 
 # Write out test solutions.
 with open(tag_file, 'r') as tag_fh:
@@ -11,16 +12,23 @@ with open(tag_file, 'r') as tag_fh:
     next(tag_csv, None)
 
     all_tags = []
+    all_tag_counts = []
     for row in tag_csv:
         for tag in row[1:]:
             if tag not in all_tags:
                 all_tags.append(tag)
+                all_tag_counts.append(1)
+            else:
+                all_tag_counts[all_tags.index(tag)] += 1
 
-    num_tags = len(all_tags)
-    print 'Done with all', num_tags, "tags!"
+    top_tag_indices = np.argpartition(np.array(all_tag_counts), -1*num_tags)[-1*num_tags:]
+    all_tags = np.array(all_tags)[top_tag_indices]
+    print np.array(all_tag_counts)[top_tag_indices]
 
     tag_fh.seek(0)
     next(tag_csv, None)
+    all_tags = all_tags.tolist()
+    print all_tags
 
     with open(bin_file, 'w') as bin_fh:
         bin_csv = csv.writer(bin_fh, delimiter=',', quotechar='"',
@@ -34,10 +42,11 @@ with open(tag_file, 'r') as tag_fh:
             bin_tags = [0] * num_tags
 
             for tag in row[1:]:
-                bin_tags[all_tags.index(tag)] = 1
+                if tag in all_tags:
+                    bin_tags[all_tags.index(tag)] = 1
 
             line = [brainz_id] + bin_tags
             bin_csv.writerow(line)
 
-            if counter%100 == 0:
+            if counter%200 == 0:
                 print "Row", counter, "done!"
