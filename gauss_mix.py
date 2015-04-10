3128280007fc480ad903936ea7ab58d1066ec717
Conrad Shock <iconshocking>
2015-04-10 06:14:34 -0400

    more gauss mix bugs fixed

--- gauss_mix.py
+++ gauss_mix.py
@@ -1,13 +1,16 @@
+from __future__ import division
 import numpy as np
 import csv
 from sklearn.mixture import GMM
 import cluster
 
-num_comps = 3
+num_comps = 12
 submit_file = 'submit_gauss_mix.csv'
 
 X, plays = cluster.get_matrix()
-X = np.array(X)
+plays = np.array(plays, dtype=float)
+X = np.array(X, dtype=float)
+X = standardizeMatrix(X)
 gauss = GMM(n_components=num_comps)
 gauss.fit(X)
 print "Weights:", gauss.weights_, "\n"
@@ -22,11 +25,14 @@ for idx, predict in enumerate(predictions):
 	plays_sums[predict] += plays[idx]
 	profiles_sums[predict] += 1
 
-for idx, profile_sum in enumerate(profile_sums):
+for idx, profile_sum in enumerate(profiles_sums):
 	plays_sums[idx] /= profile_sum
 
+print "Plays:", plays_sums
+
 Y = cluster.get_test_matrix()
-Y = np.array(Y)
+Y = np.array(Y, dtype=float)
+Y = standardizeMatrix(Y)
 test_predicts = gauss.predict(Y)
 
 with open(submit_file, 'w') as submit_fh:
@@ -35,6 +41,6 @@ with open(submit_file, 'w') as submit_fh:
     submit_csv.writerow(['Id', 'plays'])
 
     for idx, test_predict in enumerate(test_predicts):
-		submit_csv.writerow(plays_sums[test_predict])
+		submit_csv.writerow([idx+1, plays_sums[test_predict]])
 		if idx%10000 == 0:
 			print "Row", idx
