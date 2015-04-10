4c628b614e686d2d72e4041c2d63085120894a84
Wentao Xu <contactwentaoxu@gmail.com>
2015-04-10 02:53:37 -0400

    proc_test.py added

--- kmeans++.py
+++ kmeans++.py
@@ -1,31 +1,39 @@
 import csv
 import cluster
+import proc_test
 from sklearn.cluster import KMeans
 import numpy as np 
 
-#cluster.get_data()
-train_file = "train_procd_5.csv"
-with open(train_file, 'r') as train_fh:
-	train_csv = csv.reader(train_fh, delimiter=',', quotechar='"')
-	counter = 0
-	for row in train_csv:
-		row = [int(i) for i in row]
-		feat_mat = np.array(row)
-		break
+feat_mat = np.array(cluster.get_matrix())
+#print feat_mat.shape
+# train_file = "train_procd_5.csv"
+# with open(train_file, 'r') as train_fh:
+# 	train_csv = csv.reader(train_fh, delimiter=',', quotechar='"')
+# 	counter = 0
+# 	for row in train_csv:
+# 		row = [int(i) for i in row]
+# 		feat_mat = np.array(row)
+# 		break
 
-	next(train_csv, None)
-	counter = 1
-	for row in train_csv:
-		row = [int(i) for i in row]
-		feat_mat = np.vstack((feat_mat, row))
+# 	next(train_csv, None)
+# 	counter = 1
+# 	for row in train_csv:
+# 		row = [int(i) for i in row]
+# 		feat_mat = np.vstack((feat_mat, row))
 	
-		counter+=1
-		if counter % 1000 == 0:
-			print "1000"
-			break
+# 		counter+=1
+# 		if counter % 1000 == 0:
+# 			print "1000"
+# 			break
 
-print feat_mat.shape
+# print feat_mat.shape
 kmeans = KMeans()
-model = kmeans.fit(feat_mat)
+fitted_model = kmeans.fit(feat_mat)
+test_data = np.array(proc_test.get_matrix())
+fitted_model.predict(test_data)
 
-with 
+
+print results
+
+
+
